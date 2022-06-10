5a66007fd293be0cd22ed66123246378f113350e
Himanshu Vaidya <hnic07@gmail.com>
2022-06-10 12:09:43 -0400

    commit tree 3b68c4ee452623c9ee94421f710713c80bf07f3e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const { Body } = require("node-fetch");
+
+const submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json", 
+        "Accept" : "application/json", 
+    },
+    body: JSON.stringify({
+        name: "Heman",
+        email: "heman@heman.com",
+    })
+}
+
+fetch(("http://localhost:3000/users"), submitData)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        console.log(object)
+    })
+    .catch(function (error) {
+        alert ("Where the data at?");
+        console.log(error.messasge);
+    })
